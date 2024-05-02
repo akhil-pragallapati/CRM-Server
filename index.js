8f23f1d598d9da98e28c5406b5765af2e230c34d
akhil pragallapati mac <pragallapatiakhil@gmail.com>
2024-05-01 22:32:40 -0400

    CRM-Server V 1.1 - Docker CI/CD

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-//const cors = require('cors')
+const cors = require('cors')
 
 // local imports
 const connectDb = require('./db.js')
 const crmRoutes = require('./controllers/crm.controller')
-//const { errorHandler } = require('./middlewares')
+const { errorHandler } = require('./middlewares')
 
 
 const app = express()
@@ -14,18 +14,22 @@ const app = express()
 app.use(bodyParser.json())
 //app.use(cors({origin: 'http://localhost:4200'}))
 
+// for the AWS Ec2 machine public Ip where the docker image is running
+app.use(cors({origin: 'http://54.173.149.183:8081'}))
+
+
 // cores code should be before this 
 app.use('/api/crms', crmRoutes)
 
 
-//app.use(errorHandler)
+app.use(errorHandler)
 
 
 
 connectDb()
     .then(() => {
         console.log(' db connected')
-        app.listen(3000,() => console.log(' server started at 3000'))
+        app.listen(4000,() => console.log(' server started at 4000'))
 
     })
     .catch(err => console.log(err))
